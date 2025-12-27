4acd9591c29f3eaaf43a22024c99356b670cf2aa
Harish Kumar <harishkumar636r@users.noreply.github.com>
2025-12-27 22:30:47 +0530

    Initial commit - Task 1 HTML & Express server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index", { theme: "light" });
+});
+
+app.post("/submit", (req, res) => {
+  const { username, email, age } = req.body;
+  res.render("result", { username, email, age });
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log("🚀 Server running at http://localhost:3000");
+});
