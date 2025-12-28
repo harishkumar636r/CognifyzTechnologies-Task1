c5b7b87dc7b68f70796d1b23788618d82e416f44
HarishPortfolio <harishkumar636r@gmail.com>
2025-12-28 13:34:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 
-// Middleware
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Routes
+
 app.get("/", (req, res) => {
   res.render("index", { theme: "light" });
 });
