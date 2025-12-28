11703a989a8789120f9e415c1c49cde01bf4eb18
HarishPortfolio <harishkumar636r@gmail.com>
2025-12-28 13:32:12 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,18 +1,18 @@
-// Dark/Light toggle
+
 const btn = document.getElementById("toggleTheme");
 btn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   document.body.classList.toggle("light");
 });
 
-// Loader animation
+/
 const form = document.getElementById("userForm");
 const loader = document.getElementById("loader");
 form?.addEventListener("submit", () => {
   loader.style.display = "block";
 });
 
-// Floating particles
+
 for (let i = 0; i < 100; i++) {
   const dot = document.createElement("div");
   dot.classList.add("particle");
